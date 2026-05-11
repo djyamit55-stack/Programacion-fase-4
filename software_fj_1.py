b1d8824f0a8997514b4f97339d5685b0ac1f5057
djyamit55-stack <djyamit55@gmail.com>
2026-05-11 17:57:13 -0500

    Update software_fj_1.py

--- software_fj_1.py
+++ software_fj_1.py
@@ -3,6 +3,7 @@ from tkinter import messagebox, ttk # Importa diálogos de alerta y widgets mode
 from abc import ABC, abstractmethod # Importa herramientas para clases abstractas
 import logging # Importa el sistema de registro de eventos y errores
 
+
 # --- CONFIGURACIÓN DE LOGS ---
 LOG_FILE = "software_fj_debug.log" # Define el nombre del archivo donde se guardarán los errores
 logging.basicConfig(filename=LOG_FILE, level=logging.ERROR, 
@@ -10,7 +11,11 @@ logging.basicConfig(filename=LOG_FILE, level=logging.ERROR,
 
 # --- EXCEPCIONES PERSONALIZADAS ---
 class GestionError(Exception): """Excepción base para errores generales del sistema.""" 
-class DatosInvalidosError(GestionError): """Excepción para datos de entrada erróneos o faltantes.""" 
+class DatosInvalidosErrorNo(GestionError): """Excepción para datos de entrada erróneos o faltantes Nombre.""" 
+class DatosInvalidosErrorCe(GestionError): """Excepción para datos de entrada erróneos o faltantes Cedula.""" 
+class DatosInvalidosErrorCo(GestionError): """Excepción para datos de entrada erróneos o faltantes Correo.""" 
+class DatosInvalidosErrorCa(GestionError): """Excepción para datos de entrada erróneos o faltantes Cantidad.""" 
+class DatosReservaNoSeleccionada(GestionError): """Excepción específica cuando no se elige una reserva.""" 
 class ServicioNoSeleccionadoError(GestionError): """1. Excepción específica cuando no se elige un servicio válido.""" 
 class ServicioNoDisponible(GestionError): """1. Excepción cuando el servicio no esta disponible.""" 
 
@@ -26,13 +31,8 @@ class Cliente(EntidadSistema):
     """Clase que representa al cliente con encapsulamiento estricto."""
     def __init__(self, id_cliente, nombre, correo):
         self.__id = id_cliente # Identificador privado
-        self.__nombre = self.__validar_nombre(nombre) # Valida el nombre al asignar
+        self.__nombre = nombre
         self.__correo = correo # Correo electrónico privado
-
-    def __validar_nombre(self, nombre): # Método privado de validación
-        if len(nombre.strip()) < 3: # Comprueba que no esté vacío o sea muy corto
-            raise DatosInvalidosError("El nombre del cliente debe tener al menos 3 caracteres.")
-        return nombre
 
     def obtener_detalles(self): # Implementación del detalle del cliente
         return f"Cliente: {self.__nombre} (ID: {self.__id})"
@@ -97,9 +97,11 @@ class Reserva:
 # --- INTERFAZ GRÁFICA ---
 
 class VentanaSoftwareFJ:
-    tamaño = [4,6,3]   #esta tripleta indica el numero de salas, numero de equipos y acesores disponibles 
-                       #con los cuales cuenta la empresa 
-
+    
+    numeros_de_salas = 4
+    numero_de_equipos = 6
+    numero_de_asesores = 3
+    s = "salas disponibles: " + str(numeros_de_salas) + "  equipos disponibles: " + str(numero_de_equipos) + "  asesores disponibles: " + str(numero_de_asesores)
     
     
     def __init__(self, master):
@@ -130,12 +132,12 @@ class VentanaSoftwareFJ:
         frame_inferior = tk.Frame(frame_entrada, bg="grey") 
         frame_inferior.pack(side="top", fill="x", expand=True)
 
-
         tk.Label(frame_inferior, text="Servicio:").grid(row=0, column=2, padx=5) # Etiqueta servicio
-        self.combo_tipo = ttk.Combobox(frame_inferior, values=["Sala", "Equipo", "Asesoría"], width=12) # Combo de opciones
+        self.combo_tipo = ttk.Combobox(frame_inferior, values=["Salas", "Equipos", "Asesores"], width=20) # Combo de opciones
+        self.combo_tipo.config(state="readonly")
         self.combo_tipo.grid(row=0, column=3, padx=5)
 
-        tk.Label(frame_inferior, text="Cantidad:").grid(row=0, column=4, padx=5) # Etiqueta cantidad
+        tk.Label(frame_inferior, text="Cantidad horas/dias:").grid(row=0, column=4, padx=5) # Etiqueta cantidad
         self.ent_cant = tk.Entry(frame_inferior, width=8) # Campo de texto para cantidad
         self.ent_cant.grid(row=0, column=5, padx=5)
 
@@ -143,10 +145,44 @@ class VentanaSoftwareFJ:
         self.btn_reg.grid(row=0, column=6, padx=10)
 
         # ÁREA DE RESULTADOS
+        self.label = tk.Label(self.master, text=str(self.s))
+        self.label.pack(anchor="w", padx=15) # Título del historial
+
         tk.Label(self.master, text="Lista de Servicios Reservados:").pack(anchor="w", padx=15) # Título del historial
         self.txt_display = tk.Listbox(self.master, height=12) # Caja de texto multilínea
         self.txt_display.pack(fill="both", padx=15, pady=5, expand=True)
-                
+        
+        self.btn_eli = tk.Button(self.master, text="Eliminar Reserva", command=self.eliminar_registro, bg="#3498db", fg="white") # Botón registro
+        self.btn_eli.pack(pady=20)
+
+    def eliminar_registro(self):
+        try:
+            
+            # Obtener el índice del elemento seleccionado
+            seleccion = self.txt_display.curselection()
+        
+            # Si hay algo seleccionado
+            if seleccion:
+                simboloi = "¡"
+                simbolof = "!"
+                texto = self.txt_display.get(seleccion)
+                inicio = texto.find(simboloi) + 1
+                fin = texto.find(simbolof)
+                resultado = texto[inicio:fin]
+                if resultado == "S-1":
+                    self.numeros_de_salas = self.numeros_de_salas + 1
+                elif resultado == "E-1":
+                    self.numero_de_equipos = self.numero_de_equipos + 1
+                elif resultado == "A-1":
+                    self.numero_de_asesores = self.numero_de_asesores + 1
+                self.txt_display.delete(seleccion[0])
+            else: raise DatosReservaNoSeleccionada("seleccione un registro.")
+            self.s = "salas disponibles: " + str(self.numeros_de_salas) + "  equipos disponibles: " + str(self.numero_de_equipos) + "  asesores disponibles: " + str(self.numero_de_asesores)
+            self.label.config(text=str(self.s))
+        except DatosReservaNoSeleccionada as so: # Captura falta de selección
+            messagebox.showwarning("seleccione un registro", str(so))
+            logging.warning(f"Reserva no seleccionada {so}")     
+            
     def ejecutar_registro(self):
         """Método principal con manejo robusto de excepciones."""
         try:
@@ -158,51 +194,84 @@ class VentanaSoftwareFJ:
             valor_cant = self.ent_cant.get()
 
             # 1. VALIDACIÓN DE SELECCIÓN DE SERVICIO
-            if not nombre: # Si la cantidad está vacía
-                raise DatosInvalidosError("Debe ingresar una cantidad válida.")
+            if len(nombre) < 4: # Si la cantidad está vacía
+                raise DatosInvalidosErrorNo("el nombre debe tener mas de 4 caracteres")
             
-            if not cedula: # Si la cantidad está vacía
-                raise DatosInvalidosError("Debe ingresar una cantidad válida.")
+            if not cedula.isdigit(): # Si la cantidad está vacía
+                raise DatosInvalidosErrorCe("Debe ingresar un numero de cedula válida.")
             
-            if not correo: # Si la cantidad está vacía
-                raise DatosInvalidosError("Debe ingresar una cantidad válida.")
+            if not "@" in correo:
+                raise DatosInvalidosErrorCo("Debe ingresar un correo válido.")
              
             if not tipo: # Si el combo está vacío
                 raise ServicioNoSeleccionadoError("Debe seleccionar un tipo de servicio de la lista.")
 
             if not valor_cant: # Si la cantidad está vacía
-                raise DatosInvalidosError("Debe ingresar una cantidad válida.")
+                raise DatosInvalidosErrorCa("Debe ingresar una numero entero")
 
             cantidad = int(valor_cant) # Intento de conversión a entero
-
+            print("cantidad")
             # Creación de objetos de negocio
             cliente_obj = Cliente(cedula, nombre, correo)
-            
+            print("cliente creado")
             # Lógica polimórfica según el tipo de servicio
-            if tipo == "Sala":
+            if tipo == "Salas":
                 serv_obj = ReservaSala("¡S-1!", cantidad)
                 self.var = 0
-            elif tipo == "Equipo":
+            elif tipo == "Equipos":
                 serv_obj = AlquilerEquipo("¡E-1!", cantidad)
                 self.var = 1
-            elif tipo == "Asesoría":
+            elif tipo == "Asesores":
                 serv_obj = AsesoriaEspecializada("¡A-1!", cantidad)
                 self.var = 2
 
+            print(str(self.var))
             # Procesamiento de la reserva
-            if self.tamaño[self.var] == 0:
-                raise ServicioNoDisponible("Servicio no disponible.")
-            else:
-                reserva_obj = Reserva(cliente_obj, serv_obj)
-                mensaje_exito = reserva_obj.procesar()
-                self.tamaño[self.var] = self.tamaño[self.var] - 1
-                total_cobro = serv_obj.calcular_costo() # Polimorfismo en el cálculo
+            if self.var == 0:
+                if self.numeros_de_salas == 0:
+                    raise ServicioNoDisponible("Servicio no disponible.")
+            
+            if self.var == 1:
+                if self.numero_de_equipos == 0:
+                    raise ServicioNoDisponible("Servicio no disponible.")
+                
+            if self.var == 2:
+                if self.numero_de_asesores == 0:
+                    raise ServicioNoDisponible("Servicio no disponible.")
+
+            reserva_obj = Reserva(cliente_obj, serv_obj)
+            mensaje_exito = reserva_obj.procesar()
+            print("3")
+            if self.var == 0:
+                self.numeros_de_salas = self.numeros_de_salas - 1
+        
+            if self.var == 1:
+                self.numero_de_equipos = self.numero_de_equipos - 1
+
+            if self.var == 2:
+                self.numero_de_asesores = self.numero_de_asesores - 1
+            total_cobro = serv_obj.calcular_costo() # Polimorfismo en el cálculo
 
             # Salida de información exitosa
+            
+            self.s = "salas disponibles: " + str(self.numeros_de_salas) + "  equipos disponibles: " + str(self.numero_de_equipos) + "  asesores disponibles: " + str(self.numero_de_asesores)
+            self.label.config(text=str(self.s))
             self.txt_display.insert(tk.END, f"{mensaje_exito} | {serv_obj.obtener_detalles()} | Total: ${total_cobro:,.0f}\n")
 
+        except DatosInvalidosErrorNo as se: # Captura falta de selección
+            messagebox.showwarning("el nombre debe tener mas de 4 caracteres", str(se))
+            logging.warning(f"Ingreso de dato invalido: {se}")
+        except DatosInvalidosErrorCe as se1: # Captura falta de selección
+            messagebox.showwarning("Debe ingresar un numero de cedula válida", str(se1))
+            logging.warning(f"Ingreso de dato invalido: {se1}")
+        except DatosInvalidosErrorCo as se2: # Captura falta de selección
+            messagebox.showwarning("Debe ingresar un correo válido.", str(se2))
+            logging.warning(f"Ingreso de dato invalido: {se2}")
+        except DatosInvalidosErrorCa as se3: # Captura falta de selección
+            messagebox.showwarning("Debe ingresar una numero entero en cantidad", str(se3))
+            logging.warning(f"Ingreso de dato invalido: {se3}")
         except ServicioNoSeleccionadoError as sne: # Captura falta de selección
-            messagebox.showwarning("Campo Faltante", str(sne))
+            messagebox.showwarning("Campo Faltante o no valido", str(sne))
             logging.warning(f"Intento de registro sin servicio: {sne}")
         except ValueError: # Captura error de letras en campos numéricos
             messagebox.showerror("Error de Formato", "La cantidad debe ser un número entero.")
